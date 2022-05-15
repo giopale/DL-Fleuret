2e3a739e7e4ad2bf96fcf32e43b1524be28d8108
giopale <58114405+giopale@users.noreply.github.com>
2022-05-15 12:37:07 +0200

    Update model.py

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -1,5 +1,11 @@
 # Miniproject 2
 
+from torch import empty , cat , arange
+from torch.nn.functional import fold, unfold
+
+torch.set_grad_enabled(False)
+
+
 class Module (object) :
     def forward (self, *input) :
         raise NotImplementedError
