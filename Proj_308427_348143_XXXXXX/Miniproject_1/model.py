bd3c7091bfda5892ee5f18ffa4e9d7038761ba9c
giopale <58114405+giopale@users.noreply.github.com>
2022-05-25 12:42:20 +0200

    Update model.py

--- Proj_308427_348143_XXXXXX/Miniproject_1/model.py
+++ Proj_308427_348143_XXXXXX/Miniproject_1/model.py
@@ -108,7 +108,7 @@ class Model(nn.Module):
         #============================
 
         self.criterion  = nn.MSELoss()
-        self.batch_size = 16
+        self.batch_size = 20
 
         self.eta        = 0.1
         self.momentum   = 0.9
